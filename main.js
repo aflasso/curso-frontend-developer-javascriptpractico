2b4036a8216e13b9c7542d3c91a009c1bd7c30b4
aflasso <andresfelipe.lasso@gmail.com>
2022-09-04 14:29:08 -0500

    Se integra el carrito de compras en su fomra desktop y movile

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 const navEmail = document.querySelector(".navbar-email");
 const emailMenu = document.querySelector(".desktop-menu");
+const mobilMenu = document.querySelector(".mobile-menu");
+const btnmenuMovil = document.querySelector(".menu")
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
-navEmail.addEventListener("click", menuEmail)
+navEmail.addEventListener("click", toggleMenuEmail)
+btnmenuMovil.addEventListener("click", toggleMenuMobil)
+menuCarritoIcon.addEventListener("click", toogleCarritoAside)
 
-function menuEmail(event){
+function toggleMenuEmail(event){
+
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    //si el mobile menu esta open, hay que cerrarlo
+    if (!isAsideClosed) {
+            aside.classList.add("inactive")
+    }
+
     emailMenu.classList.toggle("inactive")
+}
+
+function toggleMenuMobil(event) {
+
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive")
+    }
+
+    mobilMenu.classList.toggle("inactive")
+}
+
+
+
+function  toogleCarritoAside(event){
+    const isMobilMenuClosed = mobilMenu.classList.contains("inactive");
+    const emailMenuClosed = emailMenu.classList.contains("inactive");
+
+    //si el mobile menu esta open, hay que cerrarlo
+    if (!isMobilMenuClosed || !emailMenuClosed) {
+        mobilMenu.classList.add("inactive")
+        emailMenu.classList.add("inactive")
+    }
+
+    aside.classList.toggle("inactive")
+    
 }
